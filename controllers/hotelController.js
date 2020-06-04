c8aee58c3e22a46742d2d1d944720e91a3a41e36
Elkin Huertas <mauricioelkin@live.com>
2020-06-03 20:36:23 -0400

    added feature to look at hotels by country

--- controllers/hotelController.js
+++ controllers/hotelController.js
@@ -133,3 +133,13 @@ exports.hotelDetail = async (req, res, next) => {
         next(error)
     }
 }
+
+exports.hotelByCountry = async (req, res, next) => {
+    try{
+        const countryParam = req.params.country
+        const countryList = await Hotel.find({country: countryParam});
+        res.render('hotels_by_country'. {title: `Browse by country: ${countryParam}`, countryList});
+    }catch(error){
+        next(error)
+    }
+}
