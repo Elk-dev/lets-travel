bd355deed4071849dbc4ded97a2972e143add445
Elkin Huertas <mauricioelkin@live.com>
2020-06-05 20:19:47 -0400

    created promise.all array

--- controllers/hotelController.js
+++ controllers/hotelController.js
@@ -24,15 +24,18 @@ exports.listAllCountries = async (req, res) => {
 
 exports.homePageFilters = async (req, res, next) => {
     try {
-        const hotels = await Hotel.aggregate([
+        const hotels =  Hotel.aggregate([
             { $match: { available: true }},
             { $sample: { size: 9 }}
         ]);
-        const countries = await Hotel.aggregate([
+        const countries =  Hotel.aggregate([
             { $group: { _id: '$country' }},
             { $sample: { size: 9 }}
         ]);
-        res.render('index', { countries, hotels})
+
+        const [filteredCountries, filteredHotels] = await Promise.all([countries, hotels]);
+
+        res.render('index', { filteredCountries, filteredHotels});
     }catch(error){
         next(error)
     }
